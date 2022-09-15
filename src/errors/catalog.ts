49bef5d8be3477505d48aac9766097c312e5a473
Leo Araujo <leonardo_02452@hotmail.com>
2022-09-15 16:34:34 -0300

    feat: catalogo de erros
    Criado catalogo com erros de entidade não encontrada e
    de id mongo inválido

---
+++ src/errors/catalog.ts
@@ -0,0 +1,22 @@
+export enum ErrorTypes {
+  EntityNotFound = 'EntityNotFound',
+  InvalidMongoId = 'InvalidMongoId',
+}
+
+type ErrorResponseObject = { 
+  message: string;
+  httpStatus: number
+};
+
+export type ErrorCatalog = Record<ErrorTypes, ErrorResponseObject>;
+
+export const errorCatalog: ErrorCatalog = {
+  EntityNotFound: {
+    message: 'Object not found',
+    httpStatus: 404,
+  },
+  InvalidMongoId: {
+    message: 'Id must have 24 hexadecimal characters',
+    httpStatus: 400,
+  },
+};
