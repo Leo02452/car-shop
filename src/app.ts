df78403d9db088200e5448098aaec7e353f707fa
Leo Araujo <leonardo_02452@hotmail.com>
2022-09-15 15:51:40 -0300

    feat:  app e servidor
    Foram implementadas a rota de criar carro e o middleware de erro
    Além disso foi criado o servidor da aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/errorHandle';
+import carRoute from './routes/CarRoute';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carRoute);
+app.use(errorHandler);
 
 export default app;
