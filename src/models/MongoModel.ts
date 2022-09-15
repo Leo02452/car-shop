4d286fdbb053c294e0cf07644c4a6f7001e4bd5c
Leo Araujo <leonardo_02452@hotmail.com>
2022-09-15 16:36:34 -0300

    feat: validação de id mongo no model generico

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,5 @@
 import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
 import { IModel } from '../interfaces/IModel';
 
 abstract class MongoModel<T> implements IModel<T> {
@@ -19,11 +20,13 @@ abstract class MongoModel<T> implements IModel<T> {
   }
 
   public async readOne(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
+
     return this._model.findOne({ _id });
   }
 
   public async update(_id: string, object: T): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('Not valid id');
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     
     const updated = await this._model.findByIdAndUpdate(
       { _id },
